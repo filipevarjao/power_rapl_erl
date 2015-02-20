7707a79f773fd0bd5a517a91baa006ed6a959ce2
Filipe Varjão <varjaofilipe@gmail.com>
2015-02-20 18:13:44 -0300

    erlang energy profiler

--- rapl_erl.erl
+++ rapl_erl.erl
@@ -1,36 +1,36 @@
 -module(rapl_erl).
--export([start_energy_profiling/3, check/5, bin_to_num/1,
+-export([start_energy_profiling/3, start/3, check/6, bin_to_num/1, reload/0,
   print_h/0, print_r/5]).
 
 -import(rapl_nif, [read_rapl/0]).
 
 start_energy_profiling(M, F, A) ->
+  spawn(fun() -> start(M, F, A) end).
+
+start(M, F, A) ->
   Eb = list_to_binary(read_rapl()),
   {_, Result} = timer:tc(M, F, A),
   Ea = list_to_binary(read_rapl()),
 
-  check(M, F, A, Eb, Ea),
-  R = bin_to_num(Ea) - bin_to_num(Eb),
-  Er = io_lib:format("~.6f",[R]),
-  io:format("~p~n", [Result]),
-  print_h(),
-  print_r(M, F, Eb, Ea, Er).
+  check(M, F, A, Result, Eb, Ea).
 
-check(M, F, A, Eb, Ea) ->
+check(M, F, A, Result, Eb, Ea) ->
   case Eb =:= Ea of
     false ->
-      io:fwrite("FALSE"),
-      Ea;
+      R = bin_to_num(Ea) - bin_to_num(Eb),
+      Er = io_lib:format("~.6f",[R]),
+      io:format("Result: ~p~n", [Result]),
+      print_h(),
+      print_r(M, F, Eb, Ea, Er);
     true ->
-      io:fwrite("TRUE"),
       reload(),
       start_energy_profiling(M, F, A)
   end.
 
 reload() ->
-  code:purge(rapl_nif),
-  compile:file(rapl_nif),
-  code:load_file(rapl_nif).
+  code:purge(?MODULE),
+  compile:file(?MODULE),
+  code:load_file(?MODULE).
 
 print_h() ->
   H = ["MODULE", "FUNCTION", "ENERGY BEFORE", "ENERGY AFTER", "ENERGY CONSUMED"],
